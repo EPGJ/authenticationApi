ce1ee8ee8e345da66f097743ce37e535e2bc2cfe
elias <elias.j1@hotmail.com>
2020-01-11 05:04:02 -0300

    validação do registro concluída

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,14 @@
 const router = require("express").Router()
 const User = require("../models/User")
-
-//validation
-const Joi = require("@hapi/joi")
-const schema = Joi.object({
-    name: Joi.string().min(2).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-})
+const {registerValidation} = require("../validation")
 
 
 router.post("/register", async (req, res) => {
 
-    const {error} = schema.validate(req.body)
-
+    const {error} = registerValidation(req.body)
     if(error){
         return res.status(400).send(error.details[0].message)
     }
-
     const user = new User({
         name: req.body.name,
         email: req.body.email,
